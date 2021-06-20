ca5a684bd91a1001a5d39e1ae552566ec99cd819
Arthur Servulo <arthurservulo7@gmail.com>
2021-06-20 17:32:00 -0300

    feat: models

---
+++ app/Models/Candidate.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Candidate extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'candidates';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'party',
+    ];
+
+    /**
+     * Get all of the votes for the Candidate
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function votes()
+    {
+        return $this->hasMany(Vote::class, 'candidate_id', 'id');
+    }
+
+    /**
+     * Get the number of the votes for the Candidate
+     *
+     * @return integer
+     */
+    public function getVoteCount()
+    {
+        return $this->votes->count();
+    }
+}
