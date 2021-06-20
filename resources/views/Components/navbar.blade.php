101f6bcc8c6fbc18d63153446c845e3419bad4af
Arthur Servulo <arthurservulo7@gmail.com>
2021-06-20 01:03:01 -0300

    feat: auth, admin, main, components

---
+++ resources/views/Components/navbar.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="{{route('main')}}">Votar</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Painel de Administração</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+</header>
